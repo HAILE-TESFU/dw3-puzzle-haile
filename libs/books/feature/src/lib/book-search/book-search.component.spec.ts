f529aa82d846770dc88de83d37bb8564fc5fad48
Hailezghi132@ <weldemariamhaile@gmail.com>
2020-12-10 12:36:33 -0800

    Adding test for book-search component

--- libs/books/feature/src/lib/book-search/book-search.component.spec.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.spec.ts
@@ -1,27 +1,58 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { Store } from '@ngrx/store';
+
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedTestingModule } from '@tmo/shared/testing';
 
 import { BooksFeatureModule } from '../books-feature.module';
 import { BookSearchComponent } from './book-search.component';
+import { getAllBooks } from '../../../../data-access/src/lib/+state/reading-list.selectors';
+import { By } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('ProductsListComponent', () => {
   let component: BookSearchComponent;
   let fixture: ComponentFixture<BookSearchComponent>;
+  let store: MockStore<Store>;
+  let bookListSelector;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [BooksFeatureModule, NoopAnimationsModule, SharedTestingModule]
+      imports: [BooksFeatureModule, NoopAnimationsModule, SharedTestingModule],
+      providers: [provideMockStore()],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
 
   beforeEach(() => {
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(BookSearchComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(MockStore);
+
+    bookListSelector = store.overrideSelector(getAllBooks, [
+      {
+        id: '1',
+        title: 'Advanced Angular',
+        authors: ['Haile', 'Tekle'],
+        description: 'The best angular book',
+        publisher: 'Angular University',
+        isAdded: true,
+      },
+    ]);
+
     fixture.detectChanges();
+    spyOn(store, 'dispatch').and.callFake(() => {});
   });
 
   it('should create', () => {
     expect(component).toBeDefined();
+  });
+
+  it('Should render a book list ', () => {
+    expect(
+      fixture.debugElement.queryAll(By.css('.book .book-title')).length
+    ).toBe(1);
   });
 });
